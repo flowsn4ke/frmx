c4a8446f81721596cd59864c16333cce291bbe3d
Théophile Louvel <66867063+theophilelouvel@users.noreply.github.com>
2021-08-08 19:47:03 +0200

    :books: doc setup

--- website/src/components/HomepageFeatures.js
+++ website/src/components/HomepageFeatures.js
@@ -4,38 +4,35 @@ import styles from './HomepageFeatures.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Simple',
     Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        <code>frmx</code> keeps things simple.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Performant',
     Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        <code>frmx</code> only rerenders fields that are being updated through memoization.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Lightweight',
     Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        <code>frmx</code> is only ~21kb minified and ~7kb gzipped.
       </>
     ),
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ Svg, title, description }) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
