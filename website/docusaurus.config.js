cdc25ec4a67e1cf3362c3d237493e484652b02b2
Théophile Louvel <66867063+theophilelouvel@users.noreply.github.com>
2021-08-14 01:16:26 +0200

    :art: website favicon

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -9,7 +9,7 @@ module.exports = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.svg',
   organizationName: 'theolouvel',
   projectName: 'frmx',
   themeConfig: {
