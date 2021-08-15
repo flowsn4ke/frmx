1160bd2b2aec9c40e3aad4aa243eb031691b8e2a
Théophile Louvel <66867063+theophilelouvel@users.noreply.github.com>
2021-08-15 04:29:36 +0200

    :fire: useFldX cleanup

--- src/hooks/useFldX.js
+++ src/hooks/useFldX.js
@@ -1,6 +1,5 @@
 import cloneDeep from 'lodash-es/cloneDeep'
-import { useEffect, useState, useRef } from 'react'
-
+import { useState, useRef } from 'react'
 import { useFrmX } from '../Contexts'
 
 export default function useFldX(field) {
@@ -9,31 +8,22 @@ export default function useFldX(field) {
     getOneField,
     setOneError,
     setOneField,
-    useResetListener
+    useResetListener,
   } = useFrmX()
 
   const [value, setValue] = useState(cloneDeep(getOneField(field)))
   const [error, setError] = useState(false)
 
-  const handleError = useRef((isError) => {
-    setError(isError)
-    setOneError(field, isError)
-  })
-
-  useEffect(() => {
-    handleError.current(value)
-    return () => setOneError(field, false)
-  }, [])
-
-  const handleReset = useRef(() => {
-    setValue(cloneDeep(getOneField(field)))
-    handleError.current(value)
-  })
+  const handleReset = useRef(() => setValue(cloneDeep(getOneField(field))))
   useResetListener(handleReset.current)
 
   const onChange = useRef((val) => {
     setValue(val)
     setOneField(field, val)
+  })
+  const handleError = useRef((isError) => {
+    setError(isError)
+    setOneError(field, isError)
   })
 
   return {
@@ -41,6 +31,6 @@ export default function useFldX(field) {
     setValue: onChange.current,
     error,
     setError: handleError.current,
-    disabled: formIsDisabled
+    disabled: formIsDisabled,
   }
 }
