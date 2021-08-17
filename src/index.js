d251d7b10411bd0ddd3db4b59b4bea86304bcf07
Théophile Louvel <66867063+theophilelouvel@users.noreply.github.com>
2021-08-17 23:36:36 +0200

    :fire: dev took removed from bundle

--- src/index.js
+++ src/index.js
@@ -5,6 +5,5 @@ import RstX from './RstX'
 import ArrX from './ArrX'
 import useFldX from './hooks/useFldX'
 import { useFrmX } from './Contexts'
-import { useWhyDidYouUpdate } from './hooks/useWhyDidYouUpdate'
 
-export { FrmX, FldX, ArrX, BtnX, RstX, useFrmX, useFldX, useWhyDidYouUpdate }
+export { FrmX, FldX, ArrX, BtnX, RstX, useFrmX, useFldX }
