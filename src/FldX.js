99ab7a53afdcb18c86e24eb30374c7720e9c1ab6
Théophile Louvel <66867063+theophilelouvel@users.noreply.github.com>
2021-08-17 04:58:59 +0200

    :package: useFldX API improvement

--- src/FldX.js
+++ src/FldX.js
@@ -1,8 +1,5 @@
-import cloneDeep from 'lodash-es/cloneDeep'
-import { cloneElement, Children, useEffect, useMemo, useState, useRef } from 'react'
-
-import { useFrmX, useArrX } from './Contexts'
-import { getValidationMethod } from './utils/getValidationMethod'
+import { cloneElement, Children, useRef } from 'react'
+import useFldX from './hooks/useFldX'
 
 // TODO: Trim values when submitting based on prop && if type is text
 export default function FldX({
@@ -24,61 +21,25 @@ export default function FldX({
   ...rest
 }) {
   const {
-    disabled: formIsDisabled,
-    getOneField,
-    schemaValidation,
-    setOneError,
-    setOneField,
-    useResetListener
-  } = useFrmX()
-
-  const arrx = useArrX()
-  const validationMethod = useRef(getValidationMethod(arrx, field, schemaValidation))
-
-  const [value, setValue] = useState(cloneDeep(getOneField(field)))
-  const [onceValid, setOnceValid] = useState(!validationMethod.current)
-  const [touched, setTouched] = useState(false)
-  const [error, setError] = useState(false)
-
-  const handleError = useRef((newVal) => {
-    if (!!validationMethod.current) {
-      const isError = !validationMethod.current(newVal)
-      if (!onceValid && !isError) setOnceValid(true)
-      setError(isError)
-      setOneError(field, isError)
-    }
-  })
-  useEffect(() => {
-    handleError.current(value)
-    return () => setOneError(field, false)
-  }, [])
-
-  const handleReset = useRef(() => {
-    setValue(cloneDeep(getOneField(field)))
-    handleError.current(value)
-  })
-  useResetListener(handleReset.current)
+    value,
+    setValue,
+    error,
+    disabled,
+    onBlur
+  } = useFldX(field, { afterChange, trim, disabled: locallyDisabled })
 
   const onChange = useRef((...args) => {
     let val = !!getValueFromArgs ? getValueFromArgs(args) : type === "checkbox" ? args[0].target.checked : args[0].target.value
-    val = !!trim && typeof val === 'string' ? val.trim() : val
-
     setValue(val)
-    setOneField(field, val)
-    handleError.current(val)
-
-    if (!!afterChange) afterChange(field, val)
   })
-
-  const onBlur = useRef(() => setTouched(true))
 
   const props = {
     type,
-    onBlur: onBlur.current,
+    onBlur,
     onChange: onChange.current,
-    disabled: formIsDisabled || locallyDisabled,
+    disabled,
     [type === "checkbox" ? "checked" : "value"]: value,
-    ...(isErrorProp ? { [isErrorProp]: error && (onceValid || touched) ? true : false } : {}),
+    ...(isErrorProp ? { [isErrorProp]: error } : {}),
     ...(!autoCorrectOn && { autoCorrect: "off" }),
     ...(!autoCapitalizeOn && { autoCapitalize: "none" }),
     ...(!spellCheckOn && { spellCheck: "false" }),
--- src/hooks/useFldX.js
+++ src/hooks/useFldX.js
@@ -1,38 +1,62 @@
 import cloneDeep from 'lodash-es/cloneDeep'
-import { useState, useRef } from 'react'
-import { useFrmX } from '../Contexts'
+import { useState, useRef, useEffect } from 'react'
+import { useArrX, useFrmX } from '../Contexts'
+import { getValidationMethod } from '../utils/getValidationMethod'
 
-export default function useFldX(field) {
+export default function useFldX(field, config = {}) {
   const {
     disabled: formIsDisabled,
     getOneField,
+    schemaValidation,
     setOneError,
     setOneField,
-    useResetListener,
+    useResetListener
   } = useFrmX()
 
+  const arrx = useArrX()
+  const validationMethod = useRef(getValidationMethod(arrx, field, schemaValidation))
+
   const [value, setValue] = useState(cloneDeep(getOneField(field)))
+  const [onceValid, setOnceValid] = useState(!validationMethod.current)
+  const [touched, setTouched] = useState(false)
   const [error, setError] = useState(false)
 
-  const handleReset = useRef(() => setValue(cloneDeep(getOneField(field))))
+  const handleError = useRef((newVal) => {
+    if (!!validationMethod.current) {
+      const isError = !validationMethod.current(newVal)
+      if (!onceValid && !isError) setOnceValid(true)
+      setError(isError)
+      setOneError(field, isError)
+    }
+  })
+  useEffect(() => {
+    handleError.current(value)
+    return () => setOneError(field, false)
+  }, [])
+
+  const handleReset = useRef(() => {
+    setValue(cloneDeep(getOneField(field)))
+    handleError.current(value)
+  })
   useResetListener(handleReset.current)
 
-  const onChange = useRef((val) => {
-    const newVal = typeof val === "function" ? val(value) : val
-    setValue(newVal)
-    setOneField(field, newVal)
+  const handleChange = useRef((val) => {
+    val = typeof val === "function" ? val(value) : val
+    val = !!config?.trim && typeof val === 'string' ? val.trim() : val
+    setValue(val)
+    setOneField(field, val)
+    handleError.current(val)
+
+    if (!!config?.afterChange && typeof config?.afterChange === "function") config.afterChange(field, val)
   })
-  const handleError = useRef((err) => {
-    const newErr = typeof err === "function" ? val(error) : err
-    setError(newErr)
-    setOneError(field, newErr)
-  })
+
+  const onBlur = useRef(() => setTouched(true))
 
   return {
     value,
-    setValue: onChange.current,
-    error,
-    setError: handleError.current,
-    disabled: formIsDisabled,
+    setValue: handleChange.current,
+    error: error && (onceValid || touched),
+    disabled: formIsDisabled || config?.disabled,
+    onBlur: onBlur.current,
   }
 }
